220f5f1057e185dbbfc09d87e519767fa980b2a4
Martin Oskarsson <maosk@dfds.com>
2026-03-23 17:08:08 +0100

    add schema prefixes

--- backend/deploy-scripts/migrations/108_increase_rationale_max_length.sql
+++ backend/deploy-scripts/migrations/108_increase_rationale_max_length.sql
@@ -1,3 +1,3 @@
-ALTER TABLE application_fit_scores ALTER COLUMN rationale TYPE VARCHAR(2000);
-ALTER TABLE strategy_importance ALTER COLUMN rationale TYPE VARCHAR(2000);
-ALTER TABLE enterprise_strategic_importance ALTER COLUMN rationale TYPE VARCHAR(2000);
+ALTER TABLE capabilitymapping.application_fit_scores ALTER COLUMN rationale TYPE VARCHAR(2000);
+ALTER TABLE capabilitymapping.strategy_importance ALTER COLUMN rationale TYPE VARCHAR(2000);
+ALTER TABLE enterprisearchitecture.enterprise_strategic_importance ALTER COLUMN rationale TYPE VARCHAR(2000);
